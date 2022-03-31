b6617eb5e0999f8bf8288b79a4bb68d1260b13b3
Olawale O Samson <smsnmicheal@gmail.com>
2022-03-31 21:14:35 +0100

    added mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import '../styles/NavBar.css'
 
 const NavBar = () => {
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
+
   return (
     <nav className='nav'>
         <Link to={'/'}>
@@ -27,6 +33,30 @@ const NavBar = () => {
                 <Link to={'/contact'}>Contact</Link>
             </li>
         </ul>
+
+        <div className={clicked ? 'menu-button close': 'menu-button'} onClick={handleClick}>
+            <div className="button-lines"></div>
+            <div className="button-lines"></div>
+            <div className="button-lines"></div>
+        </div>
+        <div className={clicked ? 'menu show' : 'menu'} onClick={() => setClicked(!clicked)} >
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={'/'}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={'/about'}>About</Link>
+                        </li>
+                        <li>
+                            <Link to={'/gallery'}>Gallery</Link>
+                        </li>
+                        <li>
+                            <Link to={'/contact'}>Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+        </div>
     </nav>
   )
 }
