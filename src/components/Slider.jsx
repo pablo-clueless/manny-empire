daec1afec82a757eea542704a4ce01e232d2fe14
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-04 12:41:41 +0100

    removed slider and carousel

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 import { Data } from '../../data'
 import '../styles/Slider.css'
 
+
 const Slider = () => {
   return (
     <div className='slider-container'>
         <h3>High Class Wedding Gowns</h3>
         <div className='slider'>
         {Data.map((slide, i) => (
-            <img src={slide.image} alt={slide.alt} key={i} />
+          <img src={slide.image} alt={slide.alt} key={i} />
         ))}
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,12 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import Section from '../components/Section'
-import Slider from '../components/Slider'
-// import Carousel from '../components/Carousel'
 
 const Home = () => {
   return (
     <>
     <Header />
     <NavBar />
-    <Slider />
     <Section />
     <Footer />
     </>
