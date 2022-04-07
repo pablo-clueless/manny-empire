c9dff25dc843f84008524b66ad7c9f01d38e3c6b
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-07 11:50:20 +0100

    added store memo

--- src/components/StoreHeader.jsx
+++ src/components/StoreHeader.jsx
@@ -8,6 +8,8 @@ const StoreHeader = () => {
   return (
       <header className='store-header'>
           <h1>4k Boutique by Manny</h1>
+          <p>This store is currently stocking materials, please check back later.</p>
+          <img src='/empty_cart.svg' alt="empty cart" />
       </header>
   )
 }
